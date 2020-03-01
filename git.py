00f72d648a13b09cec2db1168eb5bf1b43297226
Daniel Ellis <daniel.ellis@york.ac.uk>
2020-03-01 18:46:23 +0000

    correctc2

--- git.py
+++ git.py
@@ -3,6 +3,15 @@ import sys,os
 os.system('python clean.py')
 os.system('git add -A')
 
+
+size = os.path.getsize('thesis.pdf')/(1024*1024)
+print ('thesis size: %.2f MB'%size)
+
+if size>100:
+    print('removing thesis.pdf - its too large')
+    os.system('git rm --cached thesis.pdf')
+
+
 os.system('git commit -m "%s"'%sys.argv[1])
 print('push')
 os.system('git push')
